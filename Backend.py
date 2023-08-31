eda3655e3bd9aad699afe25fed2fba6229f03d38
gurbuzerdem94 <141748259+gurbuzerdem94@users.noreply.github.com>
2023-08-31 13:32:01 +0300

    Add files via upload

---
+++ Backend.py
@@ -0,0 +1,31 @@
+# HTTP response
+
+import requests
+import json
+
+response = requests.get('https://flights-api.buraky.workers.dev/')
+
+print(response)
+# print(response.status_code)
+
+if response.status_code != 200:
+    print("Hata!")
+
+print("\n")
+#Response Contents
+
+data = json.loads(response.content)
+
+for flight in data["data"]:
+    print("Flight {")
+    print("    Id:", flight["id"])
+    print("    From:", flight["from"])
+    print("    To:", flight["to"])
+    print("    Date:", flight["date"])
+    print("},")
+
+# Content Type
+content_type = response.headers['Content-Type']
+
+print("\n" ,content_type)
+
